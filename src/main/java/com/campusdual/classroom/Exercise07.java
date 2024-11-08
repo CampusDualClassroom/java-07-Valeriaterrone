793258386d50e4cd9ea90721b4bb626b20983b39
valeriaterrone <valeriaprogramacioncurso@gmail.com>
2024-11-08 15:45:13 +0100

    ejercicio 07 pasado todo bien, creo!

--- src/main/java/com/campusdual/classroom/Exercise07.java
+++ src/main/java/com/campusdual/classroom/Exercise07.java
@@ -9,8 +9,9 @@ public class Exercise07 {
         System.out.println("Suma de los primeros " + N + " números naturales: " + sumFirstNaturalNumbers(N));
         System.out.print("Los primeros " + N + " números naturales: ");
         showFirstNaturalNumbers(N);
-    }
+        positionInAList(8);
 
+    }
 
     // ESTE METODO NO ES NECESARIO HACERLO POR PARTE DE LOS ALUMNOS
     // Crear una List<Integer> con los números del 10 al 1. Luego buscar el elemento
@@ -18,14 +19,30 @@ public class Exercise07 {
     // Si lo encuentra, que imprima el siguiente mensaje → El elemento X se encuentra en la posición: Y
     // Si NO lo encuentra, que imprima el siguiente mensaje → El elemento X no se encuentra en la lista.
     public static void positionInAList(int num) {
-     }
+        int[] numbers = new int[11];  // Array de tamaño 11 para los números 0 a 10
 
+        for (int i = 0; i <= 10; i++) {
+            numbers[i] = i;
+        }
+        boolean found = false; //Al finalizar el bucle si found es false, significa q el número num no se encontraba en el array.
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == num) {
+                System.out.println("El elemento " + num + " se encuentra en la posición: " + i);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("El elemento " + num + " no se encuentra en la lista.");
+        }
+    }
 
     // Que imprima como mensaje la suma de los primeros N números positivos
     public static int sumFirstNaturalNumbers(int num) {
-        return (num * (num + 1)) / 2;
+        int sum = (num * (num + 1)) / 2;
+        System.out.println("La suma de los primeros " + num + " números naturales es: " + sum);
+        return sum;
     }
-
 
     // Que imprima por pantalla los N primeros números positivos
     public static void showFirstNaturalNumbers(int num) {
@@ -34,5 +51,4 @@ public class Exercise07 {
         }
         System.out.println();
     }
-
 }
